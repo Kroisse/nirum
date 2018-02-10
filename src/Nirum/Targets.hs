aa706dd028dafb23c103e3660a1c8735c0ea9460
Wonwoo Choi <chwo9843@gmail.com>
2018-02-10 22:31:25 +0900

    Add basic Rust target (WIP)

--- src/Nirum/Targets.hs
+++ src/Nirum/Targets.hs
@@ -46,6 +46,7 @@ import Nirum.Targets.List (targetProxyMapQ)
 -- docs/target/x.md file too.
 import Nirum.Targets.Docs ()
 import Nirum.Targets.Python ()
+import Nirum.Targets.Rust ()
 
 data BuildError = TargetNameError TargetName
                 | CompileError (M.Map FilePath Text)
